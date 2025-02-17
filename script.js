7b83c7ddbfc966366870595819c402e98bf1c14d
RefereeLink <referee_link@outlook.com>
2025-02-17 22:55:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ function toggleDomicilio() {
     let domicilioDiv = document.getElementById("domicilio_extra");
     domicilioDiv.style.display = document.getElementById("domicilio_diverso").value === "si" ? "block" : "none";
 }
+
+function toggleCalcio() {
+    document.getElementById("calcio_extra").style.display = document.getElementById("attualmente_gioca").value === "si" ? "block" : "none";
+}
+
+function toggleAIA() {
+    document.getElementById("aia_extra").style.display = document.getElementById("ex_aia").value === "si" ? "block" : "none";
+}
